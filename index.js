4485e06424381b45f90869b5195ae995606bb5e6
Adrienne Paquin <adriennepaquin@gmail.com>
2021-03-29 19:04:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 function deleteFromObjectByKey(object, key) {
   newObj = Object.assign({}, object)
   delete newObj[key];
-  return object;
+  return newObj;
 }
