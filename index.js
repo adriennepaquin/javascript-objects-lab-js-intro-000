5fb12b896619ba9c3a43e4800dbda2a357b57636
Adrienne Paquin <adriennepaquin@gmail.com>
2021-03-29 19:06:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function deleteFromObjectByKey(object, key) {
   return newObj;
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key) {
+function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key];
   return object;
 }
